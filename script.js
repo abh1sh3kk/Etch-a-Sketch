23c9df487d043ee681ab7d13295fb0b35b52c81b
Nobody <a9812315989@gmail.com>
2022-02-08 11:32:38 +0545

    Fix: The reload issue and set grid issue is fixed

--- script.js
+++ script.js
@@ -6,37 +6,28 @@ let reset = document.querySelector(".reset-button");
 
 
 // default is set to 5 (from index.html)
-let userChoice = slider.value; 
+let userChoice = slider.value;
 makeGrid(userChoice);
 
 
 // sets the grid according the slider value
-setGrid.addEventListener("click", ()=>{
-	location.reload();
-	makeGrid(slider.value);
-	userChoice = slider.value;
+setGrid.addEventListener("click", () => {
+  resetTheGrid();
+  makeGrid(slider.value);
+  userChoice = slider.value;
 });
 
-
-gridBox.addEventListener("mouseover", (e)=> {
-  console.log(e.target.classList.add("touched"));
-  console.log(e.target);
-});
 
 // ready to draw
-// // let trail = document.querySelectorAll(".item");
-// trail.forEach(function (currentValue) {
-//   currentValue.addEventListener("mouseover", () => {
-//     currentValue.classList.add("touched");
-//   });
-// });
+gridBox.addEventListener("mouseover", (e) => {
+  e.target.classList.add("touched");
+});
+
 
 // ready to reset
-// trail.forEach(function (currentValue) {
-//   reset.addEventListener("click", () => {
-//     currentValue.classList.remove("touched");
-//   });
-// });
+reset.addEventListener("click", resetTheGrid);
+
+
 
 // updates slider value as you are sliding
 currValue.textContent = slider.value;
@@ -44,16 +35,39 @@ slider.addEventListener("input", function () {
   currValue.textContent = this.value;
 });
 
-function makeGrid(userChoice) {
-  gridBox.setAttribute(
-    "style",
-    `grid-template: repeat(${userChoice}, 1fr) / repeat(${userChoice}, 1fr);`
-  );
 
+
+function makeGrid(userChoice) {
+
+
+  // if we dont remove the grid the new formed grids will compress the old ones reducing the size of new divs
+  removeGrid();
+
+  // create divs
   for (let i = 1; i <= userChoice ** 2; i++) {
     let item = document.createElement("div");
     item.classList.add("item");
     item.classList.add(`item${i}`);
     gridBox.appendChild(item);
   }
-}
+
+// restructure the grid
+  gridBox.setAttribute(
+    "style",
+    `grid-template: repeat(${userChoice}, 1fr) / repeat(${userChoice}, 1fr);`
+    );
+};
+  
+  function resetTheGrid() {
+    let allBoxes = document.querySelectorAll(".touched");
+    allBoxes.forEach(function (currentValue) {
+      currentValue.classList.remove("touched");
+    });
+  };
+  function removeGrid(){
+    let childToRemove = gridBox.lastElementChild;
+    while(childToRemove){
+      gridBox.removeChild(childToRemove);
+      childToRemove = gridBox.lastElementChild;
+    }
+  }
